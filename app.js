0f70109bbaa3a1b388285c103c69633caf09f771
Arsalan Qureshi <arsalanqureshi.dev@gmail.com>
2026-07-19 06:12:47 -0700

    JS Assignment

---
+++ app.js
@@ -0,0 +1,104 @@
+// Question 1: Personal Information
+
+// var name = prompt("Please Enter Your Name")
+// var age = prompt("Please Enter Your Age")
+// var city = prompt("Please Enter Your City")
+
+// console.log("Hello"+ " " + name);
+// console.log("You are"+ " " + age + " Years old");
+// console.log("You live in"+ " " + city);
+
+// ===================================================
+
+// Question 2: Sum of Two Numbers
+
+// var x = Number (prompt("Please Enter Your Number"));
+// var y = Number (prompt("Please Enter Your Again Number"));
+
+// console.log("The Sum of the Two Numbers is: " + (x + y));
+
+// ===================================================
+
+// Question 3: Basic Calculator
+
+// var num1 = Number(prompt("Please Enter Your First Number"));
+// var num2 = Number(prompt("Please Enter Your Second Number"));
+
+// console.log("Addition: " + (num1 + num2));
+// console.log("Subtraction: " + (num1 - num2));
+// console.log("Multiplication: " + num1 * num2);
+// console.log("Division: " + num1 / num2);
+
+// ===================================================
+
+// Question 4: Rectangle Area
+
+// var length = Number(prompt("Please Enter the Length of the Rectangle"));
+// var width = Number(prompt("Please Enter the Width of the Rectangle"));
+
+// console.log("The Area of the Rectangle is: " + length * width);
+
+// ===================================================
+
+// Question 5: Circle Circumference
+
+// var radius = Number(prompt("Please Enter the Radius of the Circle"));
+// console.log("The Circumference of the Circle is: " + 2 * 3.14159265 * radius);
+
+// ===================================================
+
+// Question 6: Student Marks
+
+// var studentName = prompt("Please Enter Student Name");
+// var englishMarks = Number (prompt("Please Enter English Marks"));
+// var mathMarks = Number (prompt("Please Enter Math Marks"));
+// var scienceMarks = Number (prompt("Please Enter Science Marks"));
+// var obtainedMarks = englishMarks + mathMarks + scienceMarks;
+// var totalMarks = 300;
+
+// console.log("Student Name: " + studentName);
+// console.log("Total Marks: " + totalMarks);
+// console.log("Obtained Marks: " + obtainedMarks);
+
+// ===================================================
+
+// Question 7: Age After 10 Years
+
+// var currentAge = Number(prompt("Please Enter Your Current Age"));
+// var afterYears = currentAge + 10;
+// console.log("In 10 Years, You Will Be: " + afterYears + " Years Old");
+
+// ===================================================
+
+// Question 8: Currency Conversion
+
+// var usdAmount = Number(prompt("Please Enter Amount in USD"));
+// var conversionRate = 285; // Example conversion rate
+// var convertedAmount = usdAmount * conversionRate;
+// console.log("Converted Amount PKR: " + convertedAmount);
+
+// ===================================================
+
+// Question 9: Favorite Programming Language
+
+// var name = prompt("Please Enter Your Name");
+// var language = prompt("Please Enter Your Favorite Programming Language");
+
+// console.log("Hello " + name + " Your Favorite Programming Language is " + language);
+// console.log("Keep practicing!");
+
+// ===================================================
+
+// Question 10: Simple Receipt
+
+// var productName = prompt("Please Enter Product Name");
+// var price = Number(prompt("Please Enter Product Price"));
+// var quantity = Number(prompt("Please Enter Product Quantity"));
+
+// var totalAmount = price * quantity;
+
+// console.log("Product Name: " + productName);
+// console.log("Price: " + price);
+// console.log("Quantity: " + quantity);
+// console.log("Total Amount: " + totalAmount);
+// alert("Product Name: " + productName + "\nPrice: " + price + "\nQuantity: " + quantity + "\nTotal Amount: " + totalAmount);
